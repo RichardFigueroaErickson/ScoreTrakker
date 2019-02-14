2e2db4add6cb4406a50f98ea26d8d390226748c0
Dalton Blanchard <blanchard@mines.edu>
2019-02-14 13:00:59 -0700

    Second Commit

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,3 +1,6 @@
+/*
+ * @authors: Richard Figueroa Erickson, Dalton Blanchard
+ */
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -8,30 +11,33 @@ public class ScoreTrakker {
 	private ArrayList<Student> students;
 	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 
-	public void loadDataFromFile(String fileName) throws FileNotFoundException {
+	public void loadDataFromFile(String fileName) throws FileNotFoundException {//Throw clause will throw any exceptions 
+																				//back to ProcessFiles if a files is not found.
 		int score = 0;
-		students =new ArrayList<Student>();
-		FileReader reader = new FileReader(fileName);
+		students = new ArrayList<Student>(); //Clears the array with each new file
+		FileReader reader = new FileReader(fileName); //Opens the file
 		Scanner in = new Scanner(reader);
 		while(in.hasNextLine()) {
-			String name = in.nextLine();
-			String scoreStr = in.nextLine();
+			String name = in.nextLine(); //takes in the name of the student from one line
+			String scoreStr = in.nextLine();//takes in the score from another
 			try {
 				score = Integer.parseInt(scoreStr);
-				Student pat = new Student(name, score);
+				Student pat = new Student(name, score);//new student made and added to ArrayList
 				students.add(pat);
 			} catch(NumberFormatException e) {
-				System.out.println("Error, the Student " + name + " does not have a valid score (" + scoreStr + ")");
+				System.out.println("Error, the Student " + name + " does not have a valid score (" + scoreStr + ") \n");
 			}
 		}
 		in.close();
 	}
 
 	public void printInOrder() {
-		Collections.sort(students);
+		Collections.sort(students); // sort the students by name
+		System.out.println("Student Score List" );
 		for(Student s: students) {
-			System.out.println(s);
+			System.out.println(s);//loops and prints all students
 		}
+		System.out.println();
 	}
 
 	public void processFiles(){
@@ -40,7 +46,7 @@ public class ScoreTrakker {
 				loadDataFromFile(a);
 				printInOrder();
 			} catch (FileNotFoundException e) {
-				System.out.println("File " + a + " not found");
+				System.out.println("File " + a + " not found \n");
 			}
 		}
 	}
